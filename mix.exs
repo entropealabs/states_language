76623911656f617295380b0460997d9d55771c7d
Christopher <chris@entropealabs.com>
2020-02-01 13:27:20 -0600

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule StatesLanguage.Mixfile do
   def project do
     [
       app: :states_language,
-      version: "0.2.5",
+      version: "0.2.6",
       elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
